edf0830a272bf5cf3ea1fd8d47d7a611b52ef952
yuriy <59261753+testown@users.noreply.github.com>
2022-07-02 17:33:36 +0300

    HW 13 finished

--- src/check.js
+++ src/check.js
@@ -1,12 +1,1 @@
 import { hotel } from './index.js';
-
-// console.log(`колич гостей сейчас - ${hotel.getLength()}`);
-// console.log(`колич свободных мест - ${hotel.getActualFreePlace()}`);
-// // console.log(hotel.checkInGuest());
-
-// console.log(hotel.checkInGuest('Sara', 'Konnor', '30'));
-// console.log(hotel.checkInGuest('Arnold', 'Swartzenegger', '60'));
-// console.log(hotel.checkInGuest('Anna', 'Dou', '100'));
-// console.log(hotel.checkInGuest('Anna2', 'Dou', '10'));
-
-// console.log(hotel);
